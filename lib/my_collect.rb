81f7d60b52380b31573fbfbd2c825bdf6e6b8f51
Ryan Riesenberger <rjriesenberger@gmail.com>
2018-12-07 07:10:43 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
+def my_collect (array)
+  if block_given?
+    i = 0
+    
+    returnarr = []
+    while i < array.length
+      returnarr.push(yield(array[i]))
+      i = i + 1
+    end
+  end
+  returnarr
+end
 
